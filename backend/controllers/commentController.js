4f96eea8bfe1336e49a9f9f6d9f9c347c1d7e2fb
PilYeong <pilyeooong@gmail.com>
2020-06-10 23:41:50 +0900

    ADD: comment pagination을 위한 find 로직 수정 및 new comment populate로직

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -4,27 +4,37 @@ import { Comment } from "../models/Comment";
 export const getComments = (req, res) => {
   const {
     params: { id },
-    query: { offset }
+    query: { offset },
   } = req;
   console.log(offset);
   try {
-    Comment.find({ placeId: id }).skip(parseInt(offset, 10)).limit(3).populate("creator", "nickname").exec((err, comments) => {
-      if(err){
-        return res.status(400).json({ success: false, err });
-      }
-      return res.status(200).send(comments);
-    })
-  } catch(error){
+    Comment.find({ placeId: id })
+      .skip(parseInt(offset, 10))
+      .limit(3)
+      .populate("creator", "nickname")
+      .sort({ _id: -1 })
+      .exec((err, comments) => {
+        if (err) {
+          return res.status(400).json({ success: false, err });
+        }
+        return res.status(200).send(comments);
+      });
+  } catch (error) {
     return res.status(400).json({ success: false, error });
   }
 };
 
 export const newComment = (req, res) => {
-  const comment = new Comment(req.body);
-  comment.save((err, comment) => {
+  const {
+    params: { id },
+    body: { text },
+  } = req;
+  const comment = new Comment({ creator: req.user._id, placeId: id, text });
+  comment.save( async (err, comment) => {
     if (err) {
       return res.status(400).json({ success: false, err });
     }
+    comment = await comment.populate('creator', "nickname").execPopulate();
     return res.status(200).send(comment);
   });
 };
