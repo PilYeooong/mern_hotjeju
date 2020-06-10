c943842326c920cedd0dec223cad707b785b3294
PilYeong <pilyeooong@gmail.com>
2020-06-10 16:57:31 +0900

    ADD: comment pagination

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -3,14 +3,16 @@ import { Comment } from "../models/Comment";
 
 export const getComments = (req, res) => {
   const {
-    params: { id }
+    params: { id },
+    query: { offset }
   } = req;
+  console.log(offset);
   try {
-    Comment.find({ placeId: id }).exec((err, comments) => {
+    Comment.find({ placeId: id }).skip(parseInt(offset, 10)).limit(3).populate("creator", "nickname").exec((err, comments) => {
       if(err){
         return res.status(400).json({ success: false, err });
       }
-      return res.status(200).json({ success: true, comments });
+      return res.status(200).send(comments);
     })
   } catch(error){
     return res.status(400).json({ success: false, error });
@@ -23,6 +25,6 @@ export const newComment = (req, res) => {
     if (err) {
       return res.status(400).json({ success: false, err });
     }
-    return res.status(200).json({ success: true, comment });
+    return res.status(200).send(comment);
   });
 };
