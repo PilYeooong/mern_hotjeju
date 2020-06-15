70c04d5a32946156b1ab5c321439d1a7423f5343
PilYeong <pilyeooong@gmail.com>
2020-06-15 17:57:59 +0900

    ADD: 메인화면 imageslide 추가

--- frontend/src/Components/ImageSlide.js
+++ frontend/src/Components/ImageSlide.js
@@ -1,23 +1,40 @@
-import React, { useState, useEffect } from 'react'
+import React from "react";
+import { Zoom } from "react-slideshow-image";
+
 import styled from "styled-components";
 
 const Container = styled.div`
   width: 100%;
   height: 50vh;
   background-color: lightgray;
+  & * {
+    height: 100%;
+  }
 `;
 
-const ImageBox = styled.div``;
-const ImageSlide = () => {
+const Image = styled.img`
+  width: 100%;
+`;
 
+const zoomOutProperties = {
+  duration: 5000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true,
+  scale: 0.4,
+  arrows: true,
+};
 
+const ImageSlide = ({ images }) => {
   return (
     <Container>
-      <ImageBox>
-        
-      </ImageBox>
+        <Zoom {...zoomOutProperties}>
+          {
+            images.map((each, index) => <Image key={index} src={`http://localhost:5000/${each}`} />)
+          }
+        </Zoom>
     </Container>
-  )
-}
+  );
+};
 
-export default ImageSlide
+export default ImageSlide;
