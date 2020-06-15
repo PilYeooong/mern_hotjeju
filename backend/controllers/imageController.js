094fee86c37a8da99c833d7bb141c9be6694ab05
PilYeong <pilyeooong@gmail.com>
2020-06-15 22:10:49 +0900

    메인화면에 슬라이드 할 이미지 랜덤화

--- backend/controllers/imageController.js
+++ backend/controllers/imageController.js
@@ -1,7 +1,7 @@
 import { Image } from "../models/Image";
 
 export const randomImages = async (req, res, next) => {
-  const images = await Image.find({});
+  const images = await Image.aggregate([{ $sample: { size: 4 }}]);
   let imageList = [];
   images.map(image => imageList.push(image.src));
   return res.status(200).send(imageList);
