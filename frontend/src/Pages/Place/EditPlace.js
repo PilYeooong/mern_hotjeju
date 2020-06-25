83405faf6b7b4ca68092f09b5db41b01740ef69f
PilYeong <pilyeooong@gmail.com>
2020-06-25 16:13:37 +0900

    ADD: 접근 권한에 대한 리다이렉션 시 notification message 띄우기

--- frontend/src/Pages/Place/EditPlace.js
+++ frontend/src/Pages/Place/EditPlace.js
@@ -1,14 +1,16 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { LOAD_PLACE_DETAIL_REQUEST } from "../../_Actions/types";
 
 import EditPlaceForm from "../../Components/Place/EditPlaceForm";
-import { LOAD_PLACE_DETAIL_REQUEST } from "../../_Actions/types";
+import { notification } from "antd";
+import { FrownOutlined } from "@ant-design/icons";
+
 const EditPlace = (props) => {
   const dispatch = useDispatch();
   const { userData } = useSelector((state) => state.user);
   const { placeDetail } = useSelector((state) => state.place);
   const placeId = props.match.params.placeId;
-
   useEffect(() => {
     dispatch({
       type: LOAD_PLACE_DETAIL_REQUEST,
@@ -19,6 +21,10 @@ const EditPlace = (props) => {
   useEffect(() => {
     if(userData && placeDetail){
       if(userData._id !== placeDetail.creator){
+        notification.open({
+          message: "접근 권한이 없습니다.",
+          icon: <FrownOutlined />
+        })
         props.history.push('/');
       }
     }
